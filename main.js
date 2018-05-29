435488f5b2ec06978ce6dd281d6472cf79314044
PixelGH <32110365+PixelGH@users.noreply.github.com>
2018-05-29 20:38:37 +0200

    more ftuff

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
-const json = $.getJSON('db.json');
+$.getJSON('db.json', (json) => {
+    
+    const collections = json.collections;
 
-const test = $('#test');
+    for (var collection in collections) {
+        let itemsCollection = '';
+        for (var slot in collections[collection]) {
+            let itemsSlot = '';
+            for (var item in collections[collection][slot]) {
+                itemsSlot += `<div class="item inactive" id="${collections[collection][slot][item].id}" style="background-image:url('resource/${collections[collection][slot][item].url}')"></div>`
+            }
+            itemsCollection += `<div class="main-category" title="${slot}">${itemsSlot}</div>`
+        }
+        $('#main-container').append(`<div class="main-super-category" title=${collection}>${itemsCollection}</div>`)
+    }
+})
 
-test.html(json);
+
+$(document).ready(() => {
+    $('.item').click((event) => {
+        $(event.target).addClass('active');
+        $(event.target).removeClass('inactive');
+    })
+})
