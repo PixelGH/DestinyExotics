c078c42e86c9e239b8040b795da89b35c236a3a0
PixelGH <32110365+PixelGH@users.noreply.github.com>
2018-05-29 20:51:00 +0200

    actually fixed js

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
-$.getJSON('db.json', (json) => {
+
+
+
+$(document).ready(() => {
+    $.getJSON('db.json', (json) => {
+        
+        const collections = json.collections;
     
-    const collections = json.collections;
-
-    for (var collection in collections) {
-        let itemsCollection = '';
-        for (var slot in collections[collection]) {
-            let itemsSlot = '';
-            for (var item in collections[collection][slot]) {
-                itemsSlot += `<div class="item inactive" id="${collections[collection][slot][item].id}" style="background-image:url('resource/${collections[collection][slot][item].url}')"></div>`
+        for (var collection in collections) {
+            let itemsCollection = '';
+            for (var slot in collections[collection]) {
+                let itemsSlot = '';
+                for (var item in collections[collection][slot]) {
+                    itemsSlot += `<div class="item inactive" id="${collections[collection][slot][item].id}" style="background-image:url('resource/${collections[collection][slot][item].url}')"></div>`
+                }
+                itemsCollection += `<div class="main-category" title="${slot}">${itemsSlot}</div>`
             }
-            itemsCollection += `<div class="main-category" title="${slot}">${itemsSlot}</div>`
+            $('#main-container').append(`<div class="main-super-category" title=${collection}>${itemsCollection}</div>`)
         }
-        $('#main-container').append(`<div class="main-super-category" title=${collection}>${itemsCollection}</div>`)
-    }
-
-    $(document).ready(() => {
+    
         $('.item').click((event) => {
             $(event.target).addClass('active');
             $(event.target).removeClass('inactive');
         })
     })
 })
-
-
