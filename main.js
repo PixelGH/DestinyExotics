be5ccdf89391b51330dde54a0a5746e5a3fcb912
PixelGH <32110365+PixelGH@users.noreply.github.com>
2018-05-31 21:57:48 +0200

    filters

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var itemStates = {};
 var items = {};
-var filters = {};
+var filters = [];
+var containers = []
+var superContainers = []
 
 $(document).ready(() => {
     $.getJSON('db.json', (json) => {
@@ -8,7 +10,9 @@ $(document).ready(() => {
         const collections = json.collections;
 
         for (var collection in collections) {
+            superContainers.push(collection.split(' ').join('-'))
             for (var slot in collections[collection]) {
+                containers.push(collection.split(' ').join('-') + '-' + slot.split(' ').join('-'))
                 for (var item in collections[collection][slot]) {
                     let Item = collections[collection][slot][item]
                     items[Item.id] = Item;
@@ -23,7 +27,7 @@ $(document).ready(() => {
                 for (var item in collections[collection][slot]) {
                     let Item = collections[collection][slot][item]
                     itemStatesSlot += `
-                    <div class="item inactive" id="${Item.id}" style="background-image:url('resource/${Item.url}')">
+                    <div class="item inactive tooltipped" id="${Item.id}" style="background-image:url('resource/${Item.url}')">
                         <div class="tooltip">
                             <div class="dlc-badge" style="background-image:url('${Item.dlc + '.png' || ''}')"></div>
                             <div class="tooltip-title">${Item.damage ? `<img class="damage-icon" src="${Item.damage.toLowerCase()}.png">` : ''}${Item.name}</div>
@@ -34,9 +38,9 @@ $(document).ready(() => {
                     </div>`
                     itemStates[collections[collection][slot][item].id] = 'inactive';
                 }
-                itemStatesCollection += `<div class="main-category" data-heading="${slot}">${itemStatesSlot}</div>`
+                itemStatesCollection += `<div class="main-category" id="${collection.split(' ').join('-') + '-' + slot.split(' ').join('-')}" data-heading="${slot}">${itemStatesSlot}</div>`
             }
-            $('#main-container').append(`<div class="main-super-category" data-heading="${collection}">${itemStatesCollection}</div>`)
+            $('#main-container').append(`<div class="main-super-category" id="${collection.split(' ').join('-')}" data-heading="${collection}">${itemStatesCollection}</div>`)
         }
 
         loadItems();
@@ -79,10 +83,11 @@ $(document).ready(() => {
 
         $('.filter-list-item').click((event) => {
             let item = $(event.target)
+            let key = item.data('filter-key'), value = item.data('filter-value')
             if (item.hasClass('filter-list-item-active')) {
-                delete filters[item.data('filter-key')]
+                filters.splice(filters.indexOf({key: key, value: value}), 1)
             } else {
-                filters[item.data('filter-key')] = item.data('filter-value');
+                filters.push({key: key, value: value})
             }
             filterItems();
             item.toggleClass('filter-list-item-active')
@@ -110,13 +115,28 @@ function filterItems() {
     for (item in items) {
         let Item = items[item]
         $(`#${Item.id}`).css({'display': 'block'})
-        for (filter in filters) {
-            let value = filters[filter]
-            if (Item[filter] != value) {
-                console.log(Item.id)
+        for (filter of filters) {
+            if (Item[filter.key] != filter.value) {
                 $(`#${Item.id}`).css('display', 'none')
                 break
             }
         }
     }
+    $('.main-category').show();
+    $('.main-super-category').show();
+    formatContainers();
+}
+
+function formatContainers() {
+    $('.main-category').each((index, object) => {
+        if (Object.keys($(`#${object.id}>*:visible`)).length - 2 == 0) {
+            $(`#${object.id}`).hide();
+        }
+    })
+
+    $('.main-super-category').each((index, object) => {
+        if (Object.keys($(`#${object.id}>*:visible`)).length - 2 == 0) {
+            $(`#${object.id}`).hide();
+        }
+    })
 }
